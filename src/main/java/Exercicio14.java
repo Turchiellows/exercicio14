5806ac223ed4fd3f443ccbd5c9119ee0cde15862
root <root@DELL-7559>
2024-09-18 20:52:14 -0300

    Início do projeto

---
+++ src/main/java/Exercicio14.java
@@ -0,0 +1,29 @@
+// IMPORTS
+
+import java.util.Scanner;
+
+public class Exercicio14 {
+
+	public static void main(String[] args) {
+		// INSTANTIATE
+		Scanner sc = new Scanner(System.in);
+
+		// DECLARATE
+		int n;
+		int a;
+		int result;
+
+		// COLLECT DATA
+		System.out.print("NA : ");
+		n = sc.nextInt();
+		System.out.print("AT : ");
+		a = sc.nextInt();
+
+		// OUTPUT
+		if (n > a) {
+			System.out.println("ERROR");
+		} else {
+			System.out.println("AGE : " + (a - n));
+		}
+	}
+}
