a539cfef6dd30120fddcbe164be6c9cc0395973e
root <root@DELL-7559>
2024-09-28 14:04:21 -0300

    refactor: Mudança de indentação

--- src/main/java/Exercicio14.java
+++ src/main/java/Exercicio14.java
@@ -4,26 +4,26 @@ import java.util.Scanner;
 
 public class Exercicio14 {
 
-	public static void main(String[] args) {
-		// INSTANTIATE
-		Scanner sc = new Scanner(System.in);
+   public static void main(String[] args) {
+      // INSTANTIATE
+      Scanner sc = new Scanner(System.in);
 
-		// DECLARATE
-		int n;
-		int a;
-		int result;
+      // DECLARATE
+      int n;
+      int a;
+      int result;
 
-		// COLLECT DATA
-		System.out.print("NA : ");
-		n = sc.nextInt();
-		System.out.print("AT : ");
-		a = sc.nextInt();
+      // COLLECT DATA
+      System.out.print("NA : ");
+      n = sc.nextInt();
+      System.out.print("AT : ");
+      a = sc.nextInt();
 
-		// OUTPUT
-		if (n > a) {
-			System.out.println("ERROR");
-		} else {
-			System.out.println("AGE : " + (a - n));
-		}
-	}
+      // OUTPUT
+      if (n > a) {
+         System.out.println("ERROR");
+      } else {
+         System.out.println("AGE : " + (a - n));
+      }
+   }
 }
